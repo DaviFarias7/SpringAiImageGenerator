aec944111ad834098ee4400ae0f20f78a19b4b10
davi.farias <davi.farias@unimedjp.com.br>
2024-04-24 17:53:16 -0300

    First commit

---
+++ src/main/java/com/davifarias/SpringAIImageGenerator/SpringAiImageGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.davifarias.SpringAIImageGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiImageGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiImageGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davifarias/SpringAIImageGenerator/SpringAiImageGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davifarias.SpringAIImageGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiImageGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
