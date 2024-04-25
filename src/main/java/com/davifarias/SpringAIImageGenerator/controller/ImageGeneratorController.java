d66088988040ba20b385f5b0effd8f494ac2439d
davi.farias <davi.farias@unimedjp.com.br>
2024-04-25 11:34:10 -0300

    Fix-withN=1

--- src/main/java/com/davifarias/SpringAIImageGenerator/controller/ImageGeneratorController.java
+++ src/main/java/com/davifarias/SpringAIImageGenerator/controller/ImageGeneratorController.java
@@ -7,12 +7,12 @@ import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageGeneratorController {
+
 
     @Autowired
     private ImageClient openAiImageClient;
@@ -23,7 +23,7 @@ public class ImageGeneratorController {
                 new ImagePrompt(imagePrompt,
                         OpenAiImageOptions.builder()
                                 .withQuality("hd")
-                                .withN(4)
+                                .withN(1)
                                 .withHeight(1024)
                                 .withWidth(1024).build())
 
